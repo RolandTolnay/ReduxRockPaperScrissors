9be68e2efa00c6740fdf2b8b74ad4d7b3875fe1f
Roland Tolnay <roland.ty9@gmail.com>
2017-12-11 23:04:41 +0200

    Added test for result draw

--- ReduxRockPaperScrissorsTests/ReduxRockPaperScrissorsTests.swift
+++ ReduxRockPaperScrissorsTests/ReduxRockPaperScrissorsTests.swift
@@ -7,30 +7,20 @@
 //
 
 import XCTest
+import ReSwift
 @testable import ReduxRockPaperScrissors
 
 class ReduxRockPaperScrissorsTests: XCTestCase {
+  
+  func testResultDraw() {
+    let store = Store<AppState>(reducer: weaponReducer, state: nil)
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    // Player 1 choosing weapon
+    store.dispatch(ChooseWeaponAction(weapon: .paper))
+    // Player 2 choosing weapon
+    store.dispatch(ChooseWeaponAction(weapon: .paper))
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    // Check result
+    XCTAssertEqual(store.state.result, .draw)
+  }
 }

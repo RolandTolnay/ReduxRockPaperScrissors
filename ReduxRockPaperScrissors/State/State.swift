ec5efe357b17821c8066485e8c488cff993903e7
Roland Tolnay <roland.ty9@gmail.com>
2017-12-11 23:27:24 +0200

    Added spacing to comments

--- ReduxRockPaperScrissors/State/State.swift
+++ ReduxRockPaperScrissors/State/State.swift
@@ -17,18 +17,20 @@ struct AppState: StateType {
   // Messages on main screen
   var statusMessage: Message
   var playerMessage: Message
-  // If text is dynamic based on content, it is handled by the viewcontroller
   
   // Player who moves next
   var turn: Turn
+  
   // Weapon choice of players
   var player1Play: Play
   var player2Play: Play
+  
   // Result of the match
   var result: Result?
   
   init() {
     // initial state
+    
     self.statusMessage = .prepare
     self.playerMessage = .player1Choose
     
